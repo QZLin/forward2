189415a7e17bdb767966afadf9918687c5f527b4
QZLin <qzlin01@163.com>
2023-07-15 08:54:43 +0800

    edit actions

--- update.ps1
+++ update.ps1
@@ -3,10 +3,10 @@ if (Test-Path _DEBUG) {
     git clone https://github.com/qzlin/forward2
 }
 
-# Push-Location forward2
+Push-Location forward2
 git reset HEAD~1 --hard
 git pull ../forward
 ../rename_all.ps1 .
 git commit -a -m 'rename'
 git push --force
-# Pop-Location
+Pop-Location
