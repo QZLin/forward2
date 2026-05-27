9f24a1ab8ceeb2dd9ea239c9e94a7a1a8c50cd0f
QZLin <qzlin01@163.com>
2026-05-27 06:17:31 +0000

    rename

--- forward_test.go
+++ forward_test.go
@@ -1,4 +1,4 @@
-package forward
+package forward2
 
 import (
 	"context"
@@ -68,7 +68,7 @@ func TestSetTapPlugin(t *testing.T) {
 		t.Fatal(err)
 	}
 	dnsserver.NewServer("", []*dnsserver.Config{dnsserver.GetConfig(c)})
-	f, ok := dnsserver.GetConfig(c).Handler("forward").(*Forward)
+	f, ok := dnsserver.GetConfig(c).Handler("forward2").(*Forward)
 	if !ok {
 		t.Fatal("Expected a forward plugin")
 	}
@@ -123,7 +123,7 @@ func TestForward_Regression_NoBusyLoop(t *testing.T) {
 			f.maxConnectAttempts = tc.maxAttempts
 
 			// Assume nothing is listening on this port, so the connection will be refused.
-			p := proxy.NewProxy("forward", "127.0.0.1:54321", "tcp")
+			p := proxy.NewProxy("forward2", "127.0.0.1:54321", "tcp")
 			f.SetProxy(p)
 
 			// Create a mock tracer to count the number of connection attempts.
